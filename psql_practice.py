bd3ea4746cfd0b0b5c5092eb103192562e3e752b
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-19 00:01:07 -0300

    connect postgreSQL to python

---
+++ psql_practice.py
@@ -0,0 +1,42 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# __Import Libraries__
+
+# %%
+from sqlalchemy import create_engine 
+from pandas.io import sql
+
+
+# %% [markdown]
+# __Function to Connect to the PostgreSQL__
+#
+# This function connects PostgreSQL to python and returns the query result in a dataframe
+
+# %%
+def retrieveData(query):
+    engine = create_engine(
+      'postgresql://{}:{}@{}:{}/{}?'.format("postgres", #Username
+                                                    "", #Password
+                                           "localhost", # Host
+                                                "5432", #Port
+                                          "dvdrental"), #Database
+        connect_args={"options": "-c statement_timeout=500000000"}
+    )
+    connection = engine.connect()
+    result = sql.read_sql(query,connection)
+    connection.close()
+    return result
