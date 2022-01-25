e4f1c133abc547baed246cd31d989e06ea7f770f
david-adds <55636118+david-adds@users.noreply.github.com>
2022-01-25 20:11:33 -0300

    section 7. ex: 1-11

--- psql_practice.py
+++ psql_practice.py
@@ -34,13 +34,13 @@ import pandas as pd
 # This function connects PostgreSQL to python and returns the query result in a dataframe
 
 # %%
-def retrieveData(query):
+def retrieveData(query,dbname):
     engine = create_engine(
       'postgresql://{}:{}@{}:{}/{}?'.format("postgres", #Username
                                                     "", #Password
                                            "localhost", # Host
                                                 "5432", #Port
-                                          "dvdrental"), #Database
+                                          f"{dbname}"), #Database
         connect_args={"options": "-c statement_timeout=500000000"}
     )
     connection = engine.connect()
@@ -59,7 +59,7 @@ def retrieveData(query):
 
 # %%
 SELECT_CUSTOMER_DATA="SELECT first_name, last_name, email FROM customer;"
-query = retrieveData(SELECT_CUSTOMER_DATA)
+query = retrieveData(SELECT_CUSTOMER_DATA,'dvdrental')
 query
 
 # %% [markdown]
@@ -69,7 +69,7 @@ query
 
 # %%
 AVAILABLE_RATINGS="SELECT DISTINCT(rating) FROM film;"
-query = retrieveData(AVAILABLE_RATINGS)
+query = retrieveData(AVAILABLE_RATINGS, 'dvdrental')
 query
 
 # %% [markdown]
@@ -84,7 +84,7 @@ SELECT email FROM customer
 WHERE first_name = 'Nancy' 
 AND last_name = 'Thomas'
 '''
-query = retrieveData(CUSTOMER_EMAIL)
+query = retrieveData(CUSTOMER_EMAIL,'dvdrental')
 query
 
 # %% [markdown]
@@ -95,12 +95,10 @@ query
 
 # %%
 MOVIE_DESCRIPTION = '''
-                        SELECT description FROM film
-                        WHERE title = 'Outlaw Hanky'
-                    '''
-
-# %%
-query = retrieveData(MOVIE_DESCRIPTION)
+SELECT description FROM film
+WHERE title = 'Outlaw Hanky'
+'''
+query = retrieveData(MOVIE_DESCRIPTION,'dvdrental')
 with pd.option_context('display.max_colwidth', None):
   display(query)
 
@@ -118,7 +116,7 @@ COSTUMER_PHONE_NUMBER ='''
 SELECT phone FROM address
 WHERE address = '259 Ipoh Drive';
 '''
-query = retrieveData(COSTUMER_PHONE_NUMBER)
+query = retrieveData(COSTUMER_PHONE_NUMBER,'dvdrental')
 query
 
 # %% [markdown]
@@ -133,7 +131,7 @@ SELECT customer_id, amount FROM payment
 ORDER BY payment_date ASC
 LIMIT 10;
 '''
-query = retrieveData(COSTUMER_TOP10)
+query = retrieveData(COSTUMER_TOP10,'dvdrental')
 query
 
 # %% [markdown]
@@ -148,7 +146,7 @@ SELECT title, length FROM film
 ORDER BY length ASC
 LIMIT 5;
 '''
-query = retrieveData(SHORTEST_5_MOVIES)
+query = retrieveData(SHORTEST_5_MOVIES,'dvdrental')
 query
 
 # %% [markdown]
@@ -159,7 +157,7 @@ SHORTEST_5_MOVIES ='''
 SELECT COUNT(*) FROM film
 WHERE length <=50
 '''
-query = retrieveData(SHORTEST_5_MOVIES)
+query = retrieveData(SHORTEST_5_MOVIES,'dvdrental')
 query
 
 # %% [markdown]
@@ -173,7 +171,7 @@ PAYMENT_GREATER_THAN_5USD ='''
 SELECT count(*) FROM payment
 WHERE amount > 5.00;
 '''
-query = retrieveData(PAYMENT_GREATER_THAN_5USD)
+query = retrieveData(PAYMENT_GREATER_THAN_5USD,'dvdrental')
 query
 
 # %% [markdown]
@@ -183,7 +181,7 @@ query
 CUSTOM_DIF_DISTRICTS ='''
 SELECT COUNT(DISTINCT district) FROM address;
 '''
-query = retrieveData(CUSTOM_DIF_DISTRICTS)
+query = retrieveData(CUSTOM_DIF_DISTRICTS,'dvdrental')
 query
 
 # %% [markdown]
@@ -193,7 +191,7 @@ query
 CUSTOM_DIF_DISTRICTS ='''
 SELECT DISTINCT (district) FROM address;
 '''
-query = retrieveData(CUSTOM_DIF_DISTRICTS)
+query = retrieveData(CUSTOM_DIF_DISTRICTS,'dvdrental')
 query
 
 # %% [markdown]
@@ -205,7 +203,7 @@ SELECT COUNT(*) FROM film
 WHERE rating IN ('R')
 AND replacement_cost BETWEEN 5 and 15;
 '''
-query = retrieveData(CUSTOM_DIF_DISTRICTS)
+query = retrieveData(CUSTOM_DIF_DISTRICTS,'dvdrental')
 query
 
 # %% [markdown]
@@ -223,7 +221,7 @@ SELECT staff_id, COUNT(amount) AS qty_payments_processed
 FROM payment
 GROUP BY staff_id;
 '''
-query = retrieveData(NUM_PROCESSED_PAYMENTS)
+query = retrieveData(NUM_PROCESSED_PAYMENTS,'dvdrental')
 query
 
 # %% [markdown]
@@ -238,7 +236,7 @@ FROM film
 GROUP BY rating
 ORDER BY avg_replacement_cost
 '''
-query = retrieveData(AVG_REPLACEMENT_COST)
+query = retrieveData(AVG_REPLACEMENT_COST,'dvdrental')
 query
 
 # %% [markdown]
@@ -253,7 +251,7 @@ GROUP BY customer_id
 ORDER BY SUM(amount) DESC
 LIMIT 5;
 '''
-query = retrieveData(TOP5_CUSTOMERS)
+query = retrieveData(TOP5_CUSTOMERS,'dvdrental')
 query
 
 # %% [markdown]
@@ -272,7 +270,7 @@ FROM payment
 GROUP BY customer_id
 HAVING COUNT(*) >= 40
 '''
-query = retrieveData(PLATINUM_STATUS)
+query = retrieveData(PLATINUM_STATUS,'dvdrental')
 query
 
 # %% [markdown]
@@ -286,7 +284,7 @@ WHERE staff_id IN (2)
 GROUP BY customer_id
 HAVING SUM(amount) > 100
 '''
-query = retrieveData(SPENT_100_N_ABOVE)
+query = retrieveData(SPENT_100_N_ABOVE,'dvdrental')
 query
 
 # %% [markdown]
@@ -303,7 +301,7 @@ WHERE staff_id IN (2)
 GROUP BY customer_id
 HAVING SUM(amount) >=110
 '''
-query = retrieveData(SPENT_110_N_ABOVE)
+query = retrieveData(SPENT_110_N_ABOVE,'dvdrental')
 query
 
 # %% [markdown]
@@ -314,7 +312,7 @@ SPENT_100_N_ABOVE='''
 SELECT COUNT(*) FROM film
 WHERE title like 'J%'
 '''
-#query = retrieveData(SPENT_100_N_ABOVE)
+#query = retrieveData(SPENT_100_N_ABOVE,'dvdrental')
 #query
 
 # %% [markdown]
@@ -332,7 +330,7 @@ GROUP BY first_name, last_name
 ORDER BY MAX(customer_id) DESC
 LIMIT 1;
 '''
-# query = retrieveData(SPENT_110_N_ABOVE)
+# query = retrieveData(SPENT_110_N_ABOVE,'dvdrental')
 # query
 
 # %% [markdown]
@@ -355,7 +353,7 @@ INNER JOIN customer
 ON address.address_id = customer.address_id
 WHERE address.district IN ('California')
 '''
-query = retrieveData(CALIFORNIAN_CUSTUMERS)
+query = retrieveData(CALIFORNIAN_CUSTUMERS,'dvdrental')
 query
 
 # %% [markdown]
@@ -375,7 +373,7 @@ ON film.film_id = film_actor.film_id
 WHERE first_name = 'Nick' and last_name = 'Wahlberg'
 ORDER BY film.title
 '''
-query = retrieveData(ACTOR_IN_FILMS)
+query = retrieveData(ACTOR_IN_FILMS,'dvdrental')
 query
 
 # %% [markdown]
@@ -390,7 +388,7 @@ MONTH_PAYMENT='''
 SELECT DISTINCT(TO_CHAR(payment_date, 'MONTH')) AS month_payment
 FROM payment
 '''
-query = retrieveData(MONTH_PAYMENT)
+query = retrieveData(MONTH_PAYMENT,'dvdrental')
 query
 
 # %% [markdown]
@@ -402,7 +400,156 @@ SELECT COUNT(*) AS num_monday_payments
 FROM payment
 WHERE EXTRACT(DOW FROM payment_date) = 1 
 '''
-query = retrieveData(MONDAYS_PAYMENT)
+query = retrieveData(MONDAYS_PAYMENT,'dvdrental')
+query
+
+# %% [markdown]
+# ___
+# ___
+
+# %% [markdown]
+# ### DataBase Exercises (exercises.tar)
+
+# %% [markdown]
+# ***1. How can you retrieve all the information from the cd.facilities table?***
+#
+
+# %%
+QUERY='''
+SELECT * FROM cd.facilities; 
+'''
+query = retrieveData(QUERY,'exercises')
+query
+
+# %% [markdown]
+# **2. You want to print out a list of all of the facilities and their cost to members.\
+# How would you retrieve a list of only facility names and costs?**
+
+# %%
+QUERY='''
+SELECT name,membercost FROM cd.facilities; 
+'''
+query = retrieveData(QUERY,'exercises')
+query
+
+# %% [markdown]
+# __3. How can you produce a list of facilities that charge a fee to members?__\
+# __Expected Results should have just 5 rows__
+#
+
+# %%
+QUERY='''
+SELECT * FROM cd.facilities
+WHERE membercost != 0
+'''
+query = retrieveData(QUERY,'exercises')
+query
+
+# %% [markdown]
+# __4. How can you produce a list of facilities that charge a fee to members, and that fee is less than 1/50th of the monthly maintenance cost?\
+# Return the facid, facility name, member cost, and monthly maintenance of the facilities in question.__
+# * __Result is just two rows:__
+#
+
+# %%
+QUERY='''
+SELECT facid,name,membercost,monthlymaintenance FROM cd.facilities
+WHERE membercost < 0.02*(monthlymaintenance) AND membercost NOT IN (0)
+'''
+query = retrieveData(QUERY,'exercises')
+query
+
+# %% [markdown]
+# __5. How can you produce a list of all facilities with the word 'Tennis' in their name?__
+#
+
+# %%
+QUERY='''
+SELECT * FROM cd.facilities
+WHERE name LIKE '%Tennis%'
+'''
+# query = retrieveData(QUERY,'exercises')
+# query
+
+# %% [markdown]
+# __6. How can you retrieve the details of facilities with ID 1 and 5?\
+# Try to do it without using the OR operator.__
+
+# %%
+QUERY='''
+SELECT * FROM cd.facilities
+WHERE facid IN (1,5)
+'''
+query = retrieveData(QUERY,'exercises')
+query
+
+# %% [markdown]
+# __7. How can you produce a list of members who joined after the start of September 2012?\
+# Return the memid, surname, firstname, and joindate of the members in question.\
+# Expected Result is 10 rows (not all are shown below)__
+#
+
+# %%
+QUERY='''
+SELECT memid, surname, firstname, joindate FROM cd.members
+WHERE EXTRACT( MONTH FROM joindate) IN 
+(SELECT EXTRACT( MONTH FROM joindate) AS month FROM cd.members
+ WHERE EXTRACT( MONTH FROM joindate) >= 9 AND EXTRACT( YEAR FROM joindate) IN (2012))
+'''
+query = retrieveData(QUERY,'exercises')
+query
+
+# %% [markdown]
+# __8. How can you produce an ordered list of the first 10 surnames in the members table? The list must not contain duplicates.__
+
+# %%
+QUERY='''
+SELECT DISTINCT(surname) FROM cd.members
+ORDER BY surname ASC
+LIMIT 10
+'''
+query = retrieveData(QUERY,'exercises')
+query
+
+# %% [markdown]
+# __9. You'd like to get the signup date of your last member. How can you retrieve this information?__
+
+# %%
+QUERY='''
+SELECT joindate FROM cd.members
+ORDER BY joindate DESC
+LIMIT 1
+'''
+query = retrieveData(QUERY,'exercises')
+query
+
+# %% [markdown]
+# __10. Produce a count of the number of facilities that have a cost to guests of 10 or more.__
+
+# %%
+QUERY='''
+SELECT COUNT(*) FROM cd.facilities
+WHERE guestcost >= 10
+'''
+query = retrieveData(QUERY,'exercises')
+query
+
+# %% [markdown]
+# __11. Produce a list of the total number of slots booked per facility in the month of September 2012.\
+# Produce an output table consisting of facility id and slots, sorted by the number of slots.__
+#
+#
+
+# %%
+QUERY='''
+SELECT f.facid, SUM(b.slots) FROM cd.facilities AS f
+INNER JOIN cd.bookings AS b
+ON f.facid = b.facid
+WHERE EXTRACT(MONTH FROM starttime) in (9)
+GROUP BY f.facid
+ORDER BY SUM(b.slots)
+'''
+query = retrieveData(QUERY,'exercises')
 query
 
 # %%
